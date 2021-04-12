1e1d215fe06e8b1b50fc7e80442fd62886d6a38c
Dubrefjord <Dubrefjord@protonmail.com>
2021-04-12 15:43:14 +0200

    Improved the attack script

--- sql_attack.py
+++ sql_attack.py
@@ -11,21 +11,24 @@ json_data = sys.argv[1]
 node_data = json.loads(json_data)
 
 sqlmap_command = ["sqlmap"]
-sqlmap_command.append("--batch") #Can replace with --answers [list of answers] if batch misses vulns.
+sqlmap_command.append('--answers=keep testing=Y')
+#sqlmap_command.append('testing=Y\'')
+sqlmap_command.append("--batch") #Can replace with --answers [list of answers] if batch misses vulns. (Attack more params if one is found)
 sqlmap_command.append("-v")
-sqlmap_command.append("1")
+sqlmap_command.append("0")
+sqlmap_command.append("--flush-session")
 if "url" in node_data:
   sqlmap_command.append("-u")
   sqlmap_command.append(node_data["url"])
 if "cookies" in node_data:
   sqlmap_command.append("--cookie")
-  sqlmap_command.append("&".join(node_data["cookies"]))
+  sqlmap_command.append(node_data["cookies"])
 if "data" in node_data:
   sqlmap_command.append("--data")
-  sqlmap_command.append("&".join(node_data["data"]))
+  sqlmap_command.append(node_data["data"])
 if "parameters" in node_data and node_data["parameters"] != "":
   sqlmap_command.append("-p")
-  sqlmap_command.append(",".join(node_data["parameters"]))
+  sqlmap_command.append(node_data["parameters"])
 if "method" in node_data:
   sqlmap_command.append("--forms")
   #sqlmap_command.append("--method")
